3e8da75f4baffb8c5f8eac2772c7e21fd46ea08c
unfortunatelygeek <yilingmatriarch@proton.me>
2024-07-15 18:44:59 +0530

    Added Basic AR Functionality: Camera and Introduction for Stable VitON

--- myntra/wishlist/cart.js
+++ myntra/wishlist/cart.js
@@ -1,152 +1,138 @@
+var cartarr = JSON.parse(localStorage.getItem("BagListObj")) || [];
+var wishlistItems = [];
 
+// Function to fetch wishlist items from localStorage
+function fetchWishlistItems() {
+    // Logic to filter wishlist items from cartarr
+    wishlistItems = cartarr.filter(item => item.isWishlist === true);
+    localStorage.setItem('wishlistItems', JSON.stringify(wishlistItems));
+}
 
-var cartarr = JSON.parse(localStorage.getItem("BagListObj"))||[];
-       
+// Call fetchWishlistItems function when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    fetchWishlistItems();
 
-        var itemcount =cartarr.length;
-        localStorage.setItem("itemcount",itemcount)
+    var itemcount = wishlistItems.length;
+    localStorage.setItem("itemcount", itemcount);
 
+    var MRP = wishlistItems.reduce(function(sum, item) {
+        return sum + +(item.strikedoffprice.split(" ")[1]);
+    }, 0);
+    localStorage.setItem("MRP", MRP);
 
-        var MRP =  cartarr.reduce(function(sum,a,ind){
-          return sum+ +(cartarr[ind].strikedoffprice.split(" ")[1])
-        },0);
-        localStorage.setItem("MRP",MRP)
+    var amount = wishlistItems.reduce(function(sum, item) {
+        return sum + +(item.price.split(" ")[1]);
+    }, 0);
+    localStorage.setItem("amount", amount);
 
-       
-       
-        var amount = cartarr.reduce(function(sum,a,ind){
-          return sum+ +(cartarr[ind].price.split(" ")[1])
-        },0);
-        localStorage.setItem("amount",amount)
+    var discount = MRP - amount;
+    localStorage.setItem("discount", discount);
 
-        
-       
-        var discount = MRP - amount;
-        localStorage.setItem("discount",discount)
+    document.querySelector(".amount_pay").innerText = amount;
+    document.querySelector(".filldiscount").innerText = "- " + discount;
+    document.querySelector(".totalprice").innerText = MRP;
+    document.querySelector(".pricedets").innerText = `PRICE DETAILS ( ${itemcount} Items)`;
 
-        document.querySelector(".amount_pay").innerText= amount;
-        document.querySelector(".filldiscount").innerText= "- "+discount;
-        document.querySelector(".totalprice").innerText= MRP;
-        document.querySelector(".pricedets").innerText= `PRICE DETAILS ( ${itemcount} Items)`;
+    wishlistItems.forEach(function(item, index) {
+        var box = document.createElement("div");
+        box.className = "main";
 
+        var imgbox = document.createElement("div");
 
-        cartarr.map(function(ele,ind){
-          // document.querySelector(".container").innerText = "" 
-            var box = document.createElement("div");
-            box.className ="main"
-            
+        var image = document.createElement("img");
+        image.src = item.image_url;
+        imgbox.append(image);
 
-            var imgbox = document.createElement("div");
-            
-            var image =document.createElement("img");
-            image.src = ele.image_url
-           imgbox.append(image)
+        var detailsbox = document.createElement("div");
 
-          var detailsbox = document.createElement("div");
+        var name = document.createElement("p");
+        name.innerText = item.brand;
+        name.style.fontSize = "20px";
+        name.style.marginBottom = "-8px";
 
-          var name =document.createElement("p");
-          name.innerText=ele.brand
-          name.style.fontSize="20px";
-          name.style.marginBottom ="-8px"
+        var para = document.createElement("p");
+        para.innerText = item.para;
+        para.style.color = "gray";
 
+        var price = document.createElement("span");
+        price.innerText = item.price;
 
-          var para =document.createElement("p");
-          para.innerText=ele.para ;
-          para.style.color="gray"
+        var strikedprice = document.createElement("span");
+        strikedprice.innerText = item.strikedoffprice;
+        strikedprice.style.textDecoration = "line-through";
+        strikedprice.style.color = "gray";
 
-          var price = document.createElement("span");
-          price.innerText = ele.price
+        var offer = document.createElement("span");
+        offer.innerText = item.offer;
+        offer.style.color = "red";
 
-          var strikedprice = document.createElement("span");
-          strikedprice.innerText = ele.strikedoffprice;
-          strikedprice.style.textDecoration = "line-through";
-          strikedprice.style.color ="gray";
+        var pricepara = document.createElement("p");
+        pricepara.append(price, strikedprice);
 
+        detailsbox.append(name, para, pricepara, offer);
 
-          var offer = document.createElement("span");
-          offer.innerText =ele.offer;
-          offer.style.color="red";
+        var buttonbox = document.createElement("div");
 
+        var remove = document.createElement("button");
+        remove.innerText = "REMOVE";
+        remove.addEventListener("click", function() {
+            removeitem(index);
+        });
 
-          var pricepara =document.createElement("p");
-          pricepara.append(price,strikedprice)
-          
-          detailsbox.append(name,para,pricepara,offer)
+        buttonbox.append(remove);
 
-          var buttonbox = document.createElement("div");
-          
-          var remove =document.createElement("button");
+        box.append(imgbox, detailsbox, buttonbox);
 
-          remove.innerText ="REMOVE";
-          remove.addEventListener("click",function(){
-            removeitem(ind)
-          })
+        document.querySelector(".container").append(box);
+    });
+});
 
-          
-          
-          buttonbox.append (remove)
-          
-          
-          
-          box.append(imgbox,detailsbox,buttonbox)
+// Function to remove item from wishlist
+function removeitem(index) {
+    wishlistItems.splice(index, 1);
+    localStorage.setItem("wishlistItems", JSON.stringify(wishlistItems));
+    window.location.href = "cart.html"; // Redirect back to cart page after removal
+}
 
-        document.querySelector(".container").append(box)
+// Function to apply discount
+document.querySelector(".apply").addEventListener("click", discountfun);
 
-        })
+function discountfun() {
+    var payable_amount = +(localStorage.getItem("amount"));
+    var int_promo = document.querySelector("#promo").value;
 
-        document.querySelector(".wishlistlink").addEventListener("click",sendtowish)
-        function sendtowish (){
-          window.location.href ="wishlist.html"
-        }
+    if (payable_amount > 300 && int_promo === "MYNTRA300") {
+        amount = amount - 300;
+        discount = discount + 300;
+        localStorage.setItem("amount", amount);
+        localStorage.setItem("discount", discount);
+        document.querySelector(".amount_pay").innerText = amount;
+        document.querySelector(".filldiscount").innerText = "- " + discount;
 
+        document.querySelector(".apply").removeEventListener("click", discountfun);
+    }
+}
 
-          function removeitem(ind){
-          cartarr.splice(ind,1);
-          localStorage.setItem("BagListObj",JSON.stringify(cartarr))
-          window.location.href ="cart.html";
-         
-        }
+// Function to redirect to wishlist page
+document.querySelector(".wishlistlink").addEventListener("click", sendToLiveTryOn);
 
-        document.querySelector(".makeorder").addEventListener("click",profile)
+function sendToLiveTryOn() {
+    // Store wishlist items in localStorage and redirect to live-try-on.html
+    localStorage.setItem("wishlistItems", JSON.stringify(wishlistItems));
+    window.location.href = "myntra/AR-page/live-try-on.html";
+}
 
-        function profile(){
-          window.location.href="../Profile/signup.html";
-        }
+// Event listeners for other links/buttons (not directly related to wishlist functionality)
+document.querySelector(".makeorder").addEventListener("click", profile);
 
-        document.querySelector(".apply").addEventListener("click",discountfun);
+function profile() {
+    window.location.href = "../Profile/signup.html";
+}
 
-        function discountfun(){
+document.getElementById('landingPage').addEventListener('click', function() {
+    window.location.href = "../Landingpage/index.html";
+});
 
-          var payable_amount = +(localStorage.getItem("amount"));
-          var int_promo = document.querySelector("#promo").value;
-
-          if( payable_amount>300 && int_promo=="MYNTRA300")
-           {
-          amount = amount-300;
-          discount =discount+300;
-          localStorage.setItem("amount",amount)  ;
-          localStorage.setItem("discount",discount)
-          document.querySelector(".amount_pay").innerText= amount;
-          document.querySelector(".filldiscount").innerText= "- "+discount;
-
-          document.querySelector(".apply").removeEventListener("click",discountfun);
-           }
-            // console.log( amount, discount)
-
-        }
-
-document.getElementById('landingPage').addEventListener('click', function(){
-  window.location.href = "../Landingpage/index.html";
-
-}) 
-
-document.querySelector("#second").addEventListener("click",function(){
-  window.location.href="../Profile/signup.html";
-})
-
-
-
-        
-
-      
-
+document.querySelector("#second").addEventListener("click", function() {
+    window.location.href = "../Profile/signup.html";
+});
